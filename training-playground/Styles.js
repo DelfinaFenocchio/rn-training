81982f2111dfde807b54389ea950dcbd93a9b351
DelfinaFenocchio <delfifenocchio@gmail.com>
2019-05-23 03:36:42 -0300

    ej1

---
+++ training-playground/Styles.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		flexDirection: 'column',
+		backgroundColor: '#1190FF'
+	},
+	box: {
+		margin: 7,
+		marginBottom: 10,
+	},
+	box1: {
+		height: 100,
+		width: 100,
+		backgroundColor: 'red',
+	},
+	box2: {
+		height: 150,
+		width: 150,
+		backgroundColor: 'purple',
+	},
+	box3: {
+		height: 200,
+		width: 200,
+		backgroundColor: 'white',
+	},
+});
+export default styles;
