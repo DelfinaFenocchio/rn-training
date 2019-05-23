7575aae59514be5507d4bc80674714919526a657
DelfinaFenocchio <delfifenocchio@gmail.com>
2019-05-23 00:52:02 -0300

    ej4

--- training-playground/App.js
+++ training-playground/App.js
@@ -7,33 +7,22 @@ export default class App extends React.Component {
       <ScrollView horizontal={true}>
         <View style={styles.container}>
           <View style={styles.container1} >
-            <Text>Container1</Text>
+            <Text style={styles.text3}>text1
+              <Text style={styles.text1}>
+                text1text1text1text1text1
+              </Text>
+            </Text>
           </View>
           <View style={styles.container2} >
-            <Text>Container2</Text>
+            <Text style={styles.text2}>text2text2text2text2text2</Text>
           </View>
           <View style={styles.container3} >
-            <Text>Container3</Text>
+            <Text style={styles.text1}>text3
+              <Text style={styles.text3}>
+                text1text1text1text1text1
+              </Text>
+            </Text>
           </View>
-          <View style={styles.container1} >
-            <Text>Container1</Text>
-          </View>
-          <View style={styles.container2} >
-            <Text>Container2</Text>
-          </View>
-          <View style={styles.container3} >
-            <Text>Container3</Text>
-          </View>
-          <View style={styles.container1} >
-            <Text>Container1</Text>
-          </View>
-          <View style={styles.container2} >
-            <Text>Container2</Text>
-          </View>
-          <View style={styles.container3} >
-            <Text>Container3</Text>
-          </View>
-
         </View>
       </ScrollView>
     );
@@ -43,26 +32,38 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: 'column',
     borderRadius: 4,
     alignItems: 'center',
     justifyContent: 'center',
   },
   container1: {
     backgroundColor: '#dda0dd',
-    width: 70,
-    height: 100,
+    width: 100,
+    height: 130,
   },
   container2: {
     backgroundColor: '#ffc0cb',
-    width: 100,
-    height: 130,
+    width: 130,
+    height: 160,
     borderRadius: 9,
   },
   container3: {
     backgroundColor: '#b0e0e6',
-    width: 130,
-    height: 160,
+    width: 160,
+    height: 190,
+  },
+  text1: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  text2: {
+    fontSize: 30,
+    fontWeight: '300',
+  },
+  text3: {
+    fontSize: 45,
+    fontWeight: '500',
   },
 
 });
