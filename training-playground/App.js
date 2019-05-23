a1346d0fcc8f85530dfa865bef311c0bcbcec7f7
DelfinaFenocchio <delfifenocchio@gmail.com>
2019-05-23 01:31:32 -0300

    ej5

--- training-playground/App.js
+++ training-playground/App.js
@@ -1,30 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <ScrollView horizontal={true}>
-        <View style={styles.container}>
-          <View style={styles.container1} >
-            <Text style={styles.text3}>text1
-              <Text style={styles.text1}>
-                text1text1text1text1text1
-              </Text>
-            </Text>
-          </View>
-          <View style={styles.container2} >
-            <Text style={styles.text2}>text2text2text2text2text2</Text>
-          </View>
-          <View style={styles.container3} >
-            <Text style={styles.text1}>text3
-              <Text style={styles.text3}>
-                text1text1text1text1text1
-              </Text>
-            </Text>
-          </View>
-        </View>
-      </ScrollView>
+      <View style={styles.container}>
+        <Image style={styles.image} source={require('./pictures/paisaje.jpg')} />
+        <Image source={{ uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png' }}
+          style={{ width: 100, height: 130 }} />
+      </View>
     );
   }
 }
@@ -36,34 +20,12 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'blue',
+    width: 200,
+    height: 200,
   },
-  container1: {
-    backgroundColor: '#dda0dd',
-    width: 100,
-    height: 130,
-  },
-  container2: {
-    backgroundColor: '#ffc0cb',
-    width: 130,
-    height: 160,
-    borderRadius: 9,
-  },
-  container3: {
-    backgroundColor: '#b0e0e6',
-    width: 160,
-    height: 190,
-  },
-  text1: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  text2: {
-    fontSize: 30,
-    fontWeight: '300',
-  },
-  text3: {
-    fontSize: 45,
-    fontWeight: '500',
-  },
-
+  image: {
+    width: 150,
+    height: 120,
+  }
 });
