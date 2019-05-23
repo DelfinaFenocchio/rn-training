af226b0c0f4bbc06ad7f1c76b01874089f2c2b2a
DelfinaFenocchio <delfifenocchio@gmail.com>
2019-05-23 04:15:13 -0300

    ej5

--- training-playground/Styles.js
+++ training-playground/Styles.js
@@ -3,26 +3,25 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		flexDirection: 'column',
+		flexDirection: 'row',
 		backgroundColor: '#1190FF',
 		justifyContent: 'space-around',
-		alignItems: 'flex-end',
 	},
 	box: {
 		margin: 10,
 		width: 100,
-		alignContent: 'flex-start',
+		height: 100,
 	},
 	box1: {
-		height: '50%',
+		alignSelf: 'flex-start',
 		backgroundColor: 'red',
 	},
 	box2: {
-		height: '30%',
+		alignSelf: 'center',
 		backgroundColor: 'white',
 	},
 	box3: {
-		height: '20%',
+		alignSelf: 'flex-end',
 		backgroundColor: 'purple',
 	},
 });
